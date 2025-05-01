86b12120e02d1ba735dfa360bf1f4eed6fc5253c
Asecave <39377667+Asecave@users.noreply.github.com>
2025-05-01 18:11:11 +0200

    Sprint Fix
    Fixed starting to sprint mid air not activating rockts

--- src/main/java/com/vice/balancedflight/mixins/ElytraRocketShiftKeyMixin.java
+++ src/main/java/com/vice/balancedflight/mixins/ElytraRocketShiftKeyMixin.java
@@ -4,8 +4,11 @@ package com.vice.balancedflight.mixins;
 import com.vice.balancedflight.content.flightAnchor.FlightController;
 import com.vice.balancedflight.foundation.config.BalancedFlightConfig;
 import com.vice.balancedflight.foundation.network.CustomNetworkMessage;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.Input;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.level.Level;
+import org.lwjgl.system.windows.INPUT;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,12 +32,12 @@ public class ElytraRocketShiftKeyMixin
             if (!allowed.canElytraFly())
                 return;
 
-            if (player.isSprinting() && player.input.hasForwardImpulse())
+            if (Minecraft.getInstance().options.keySprint.isDown() && player.input.hasForwardImpulse())
             {
                 Level world = player.level();
 
-                long now = Instant.now().getEpochSecond();
-                if (now - LastUsedFireworkTime > 1)
+                long now = Instant.now().toEpochMilli();
+                if (now - LastUsedFireworkTime > 1000)
                 {
                     CustomNetworkMessage.Send(world, player, "FIRE_ROCKET");
                     LastUsedFireworkTime = now;
